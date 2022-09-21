14f5d76c1f67cef489681deac6f8c36ab6a1ee99
Ivy Nguyen <animeweirdo24@gmail.com>
2022-09-21 11:32:46 -0600

    Add stylink and back button to details page

--- src/Componenets/AmiiboDetails/AmiiboDetails.js
+++ src/Componenets/AmiiboDetails/AmiiboDetails.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import "./AmiiboDetails.css"
+import waddleDee from "../Images/waddleDee.png"
+import { Link } from 'react-router-dom'
 
 const AmiiboDetails = ({amiibo}) => {
     const [threeDsFeat, setThreeDsFeat] = useState([])
@@ -10,7 +13,7 @@ const AmiiboDetails = ({amiibo}) => {
         .then(res => res.json())
         .then(data => {
             const specificAmiiboFeats = data.amiibo.find(character => character.tail === amiibo.tail)
-            console.log('specidic amiibo feats: ', specificAmiiboFeats)
+            // console.log('specidic amiibo feats: ', specificAmiiboFeats)
             setThreeDsFeat(specificAmiiboFeats.games3DS)
             setSwitchFeat(specificAmiiboFeats.gamesSwitch)
             setWiiUFeat(specificAmiiboFeats.gamesWiiU)
@@ -19,14 +22,14 @@ const AmiiboDetails = ({amiibo}) => {
     
     const releaseDateKeys = Object.keys(amiibo.release)
     const releaseDate = releaseDateKeys.map(key => {
-        return <p key={key}> {key}: {amiibo.release[key]}</p>
+        return <li key={key}> {key}: {amiibo.release[key]}</li>
     })
 
     const features = (console) => {
         const feats = console.map(feat => {
             // console.log('use', console.log(threeDsFeat))
             const {amiiboUsage, gameName} = feat
-            return <p key={Math.random()}> {amiiboUsage[0].Usage} in {gameName}</p>
+            return <li key={Math.random()}> {amiiboUsage[0].Usage} in {gameName}</li>
         })
         return feats
     }
@@ -36,7 +39,13 @@ const AmiiboDetails = ({amiibo}) => {
     }, [])
 
     return (
-        <div>
+        <div className="amiibo-details-container">
+            <div className="back-button-container">
+                {/* <button className="back-button"> */}
+                    <Link to={`/`} className="link"> Go Back Home<img className="waddle-icon"  src={waddleDee} alt="back icon"/></Link>
+                {/* </button> */}
+            </div>
+        <div className="amiibo-details">
             <div>
                 <img src={amiibo.image} alt={amiibo.name}/>
                 <h2>{amiibo.name}</h2>
@@ -44,22 +53,31 @@ const AmiiboDetails = ({amiibo}) => {
                     Series: {amiibo.amiiboSeries}
                 </p>
                 <p>Release Dates:</p>
-                {releaseDate}
+                <ul>
+                    {releaseDate}
+                </ul>
             </div>
             <div className="amiibo-features">
                 <div className="ds-features">
                     <h2>3DS unlockables</h2>
-                    {features(threeDsFeat)}
+                    <ul>
+                        {features(threeDsFeat)}
+                    </ul>
                 </div>
                 <div className="switch-features">
                     <h2>Switch unlockables</h2>
-                    {features(switchFeat)}
+                    <ul>
+                        {features(switchFeat)}
+                    </ul>
                 </div>
                 <div className="wiiU-features">
                     <h2>WiiU unlockables</h2>
-                    {features(wiiUFeat)}
+                    <ul>
+                        {features(wiiUFeat)}
+                    </ul>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
--- src/Componenets/Header/Header.js
+++ src/Componenets/Header/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
         <div className="header">
             <h1>AmiiWho?</h1>
             <nav>
-                <button><NavLink to="/" className="nav">Home</NavLink></button>
-                <button><NavLink to="/amiiWho/myCollection" className="nav">View My Collection</NavLink></button>
-                <button><NavLink to="/amiiWho/aboutUs" className="nav">About Us</NavLink></button>
+                <button className="nav-button"><NavLink to="/" className="nav">Home</NavLink></button>
+                <button className="nav-button"><NavLink to="/amiiWho/myCollection" className="nav">View My Collection</NavLink></button>
+                <button className="nav-button"><NavLink to="/amiiWho/aboutUs" className="nav">About Us</NavLink></button>
             </nav>
         </div>
     )
